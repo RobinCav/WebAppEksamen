cb86f6d518445508012ecd3b0c25d04c357451f4
SerooshSaba <serooshsaba@gmail.com>
2021-12-01 21:41:14 +0100

    Byttet ut for loops med reduce

--- testing/__tests__/ColorPicker.test.js
+++ testing/__tests__/ColorPicker.test.js
@@ -5,7 +5,6 @@
 import { fireEvent, screen, waitFor } from '@testing-library/dom'
 import { render } from '@testing-library/react'
 import ColorPicker from '../components/ColorPicker'
-import userEvent from '@testing-library/user-event'
 
 const listOfColors = ["red", "green", "blue"];
 
@@ -23,46 +22,33 @@ describe('ColorPicker', () => {
 
   it('should have disabled button if color does not match', () => {
     render(<ColorPicker colors={listOfColors} selectedColor="purple" handleSelectedColor={()=>{}} />)
-    var buttons = document.getElementsByTagName("button")
-    var disabledButtons = 0
-    for ( var i = 0; i < listOfColors.length; i++ ) {
-      if ( buttons[i].disabled == true )
-        disabledButtons++
-    }
-    expect( disabledButtons ).toBe( listOfColors.length ) // Expect all buttons to be disabled
+    var buttons = Array.from(document.querySelectorAll("button"))
+    var disabledButtons = buttons.filter( button => button.disabled )    
+    expect( disabledButtons.length ).toBe( listOfColors.length ) // No match, expect all buttons to be disabled
   })
 
   it('should have one active button if color match', () => {
     render(<ColorPicker colors={listOfColors} selectedColor="red" handleSelectedColor={()=>{}} />)
-    var buttons = document.getElementsByTagName("button")
-    var activeButtons = 0;
-    for ( var i = 0; i < listOfColors.length; i++ ) {
-      if ( buttons[i].disabled == false )
-        activeButtons++
-    }
-    expect( activeButtons ).toBe( 1 )
+    var buttons = Array.from(document.querySelectorAll("button"))
+    var activeButtons = buttons.filter( button => !button.disabled )
+    expect( activeButtons.length ).toBe( 1 )
   })
 
   it('should have called onClick on button', async () => {
     const handleClick = jest.fn()
     render(<ColorPicker colors={listOfColors} selectedColor="" handleSelectedColor={handleClick} />)
-    var buttons = document.getElementsByTagName("button")
+    var buttons = screen.getAllByRole("button")
     fireEvent.click( buttons[0] ) // Click on active button
-    await waitFor( () => {
-      expect(handleClick).toHaveBeenCalledTimes(1)
-    })
+    await waitFor(() => expect(handleClick).toHaveBeenCalledTimes(1) )
   })
   
   it('should not have called onClick on disabled button', async () => {
     const handleClick = jest.fn()
     render(<ColorPicker colors={listOfColors} selectedColor="red" handleSelectedColor={handleClick} />)
-    var buttons = document.getElementsByTagName("button")
-    fireEvent.click( buttons[1] ) // Click on disabled green button
-    await waitFor( () => {
-      expect(handleClick).toHaveBeenCalledTimes(0)
-    })
+    var buttons = screen.getAllByRole("button")
+    fireEvent.click( buttons[1] ) // Click on disabled button
+    await waitFor(() => expect(handleClick).toHaveBeenCalledTimes(0) )
   })
-
 
   /*
   it('should updated selectedColor and active buttons on click', async () => {
@@ -75,7 +61,6 @@ describe('ColorPicker', () => {
     ///fireEvent.click( buttons[0] )
     userEvent.click( buttons[0] )
 
-
     await waitFor( () => { 
       expect( buttons[2].disabled ).toBe( true )
     })
@@ -83,6 +68,5 @@ describe('ColorPicker', () => {
 
   })
   */
-
 
 })
--- testing/__tests__/Stepper.test.js
+++ testing/__tests__/Stepper.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen } from '@testing-library/dom'
+import { fireEvent, screen, waitFor } from '@testing-library/dom'
 import { render } from '@testing-library/react'
 import Stepper from '../components/Stepper'
 
@@ -10,30 +10,27 @@ describe('Stepper component', () => {
 
   it('should render button', () => {
     render(<Stepper/>)
-    const button = document.querySelector('button')
-    expect(button).toBeInTheDocument()
+    expect(screen.getByRole('button')).toBeInTheDocument()
   })
 
   it('should have correct text content on button', () => {
     render(<Stepper/>)
-    const button = document.querySelector('button')
-    expect(button).toHaveTextContent("Game")
+    expect(screen.getByRole('button')).toHaveTextContent("Game")
   })
   
   it('should update step-count and button content on click', async () => {
     render(<Stepper/>)
-    const button = document.querySelector('button')
-    expect( button ).toHaveTextContent("Game")
-    fireEvent.click(screen.getByText('Game'))
-    expect( button ).toHaveTextContent("End")
+    fireEvent.click(screen.getByRole('button'))
+    await waitFor(() => expect( screen.getByRole('button') ).toHaveTextContent("End") )
   })
   
   it('should remove button when step count is higher than amount of steps', async () => {
     render(<Stepper/>)
-    var button = screen.getByRole("button",{type:/button/i})
-    for ( var i = 0; i < 2; i++ )
-      fireEvent.click(button)
-    expect( button ).not.toBeInTheDocument()
+    const button = screen.getByRole("button")
+    fireEvent.click(button)
+    await waitFor(() => expect(button).toHaveTextContent("End") )
+    fireEvent.click(button)
+    await waitFor(() => expect(button).not.toBeInTheDocument() )
   })
 
 })
